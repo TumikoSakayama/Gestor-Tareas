468a1b279907268fe9945ee7c997836fd5d1f8ef
TumikoSakayama <glee310597@gmail.com>
2026-01-18 13:06:34 -0600

    Translating code to english

--- main.py
+++ main.py
@@ -9,159 +9,159 @@ try:
 except Exception:
     pass
 
-TASK_FILES = "tareas.json"
-tareas = []
-contador_id = 0
-hoy = date.today()
+TASK_FILES = "tasks.json"
+tasks = []
+id_count = 0
+today = date.today()
 
-def cargar_tareas():
-    global tareas, contador_id
+def load_tasks():
+    global tasks, id_count
     if os.path.exists(TASK_FILES):
         try:
-            with open(TASK_FILES, 'r', encoding='utf-8') as archivo:
-                datos = json.load(archivo)
+            with open(TASK_FILES, 'r', encoding='utf-8') as file:
+                data = json.load(file)
 
-                if isinstance(datos, list):
-                    viejas_tareas = datos
-                    contador_id = max((tarea["id"] for tarea in tareas), default=0)
-                    print("Se detecto formato antiguo. Migrando....")
+                if isinstance(data, list):
+                    old_tasks = data
+                    id_count = max((task["id"] for task in tasks), default=0)
+                    messagebox.showinfo("Migration", "OId tasks are being migrated to the new format")
                 else:
-                    viejas_tareas = datos.get("tareas", [])
-                    contador_id = datos.get("ultimo_id", 0)
+                    old_tasks = data.get("tasks", [])
+                    id_count = data.get("last_id", 0)
 
-                migrado = False
-                for tarea in tareas:
-                    if "finalizar" not in tarea:
-                        tarea["finalizar"] = str(date.today())
-                        migrado = True
+                migrated = False
+                for task in tasks:
+                    if "completion" not in task:
+                        task["completion"] = str(date.today())
+                        migrated = True
                     
-                if migrado:
-                    guardar_tareas()
+                if migrated:
+                    save_tasks()
 
         except Exception as e:
-            messagebox.showerror("Error de Carga", f"No se pudo cargar el archivo: {e}")
+            messagebox.showerror("File Error", f"Unable to open file: {e}")
     else:
-        tareas = []
-        contador_id = 0
+        tasks = []
+        id_count = 0
 
 
-def guardar_tareas():
+def save_tasks():
     try:
-        with open(TASK_FILES, 'w', encoding='utf-8') as archivo:
-            datos_a_guardar = {
-                "ultimo_id": ultimo_id,
-                "tareas": tareas
+        with open(TASK_FILES, 'w', encoding='utf-8') as file:
+            data_to_save = {
+                "last_id": last_id,
+                "tasks": tasks
             }
-            json.dump(datos_a_guardar, archivo, indent=4, ensure_ascii=False)
-            print("Tareas guardadas correctamente")
+            json.dump(data_to_save, file, indent=4, ensure_ascii=False)
+            messagebox.showinfo("Succesfull", "Tasks were saved successfully!")
     except Exception as e:
-        messagebox.showerror("Error al guardar", f"No se pudo guardar la tarea ingresada en el archivo: {e}")
+        messagebox.showerror("Save Error", f"Unable to save task at file: {e}")
 
-def agregar_tarea():
-    global contador_id
+def add_task():
+    global id_count
 
-    descripcion = simpledialog.askstring("Nueva Tarea", "Descripcion de la tarea: ")
-    if not descripcion:
+    description = simpledialog.askstring("New Task", "Task Description: ")
+    if not description:
         return
-    prioridad = simpledialog.askstring("Prioridad", "Prioridad (Alta/Media/Baja)") or "Media"
-    prioridad = prioridad.capitalize()
-    categoria = simpledialog.askstring("Categoria", "Categoria: ") or "Sin Especificar"
+    priority = simpledialog.askstring("Priority", "Priority (Low/Medium/High)") or "Medium"
+    priority = priority.capitalize()
+    category = simpledialog.askstring("Categoria", "Categoria: ") or "Sin Especificar"
 
-    contador_id += 1
+    id_count += 1
 
-    dias_limites = {
-        "Alta": 1,
-        "Media": 5,
-        "Baja": 10
+    limit_days = {
+        "High": 1,
+        "Medium": 5,
+        "Low": 10
     }
 
-    dias_completar = dias_limites.get(prioridad, 2)
-    fecha_limite = hoy + timedelta(days=dias_completar)
+    completion_days = limit_days.get(priority, 2)
+    end_date = today + timedelta(days=completion_days)
 
-    nueva_tarea = {
-        "id": contador_id,
-        "descripcion": descripcion,
-        "prioridad": prioridad,
-        "categorias": categoria.capitalize(),
-        "completado": False,
-        "fecha": str(date.today()),
-        "finalizar": str(fecha_limite)
+    new_task = {
+        "id": id_count,
+        "description": description,
+        "priority": priority,
+        "category": category.capitalize(),
+        "completed": False,
+        "date": str(date.today()),
+        "completion": str(fecha_limite)
     }
 
-    tareas.append(nueva_tarea)
-    guardar_tareas()
-    actualizar_lista()
-    messagebox.showinfo("Exito", f"Tarea #{contador_id} agregada correctamente!")
+    tasks.append(new_task)
+    save_tasks()
+    update_task()
+    messagebox.showinfo("Successfull", f"Task #{id_count} added successfully!")
 
 
-def marcar_completada():
-    seleccion = lista_tareas.curseselection()
-    if not seleccion:
-        messagebox.showwarning("Atencion", "Seleccione una tarea de la lista, el valor seleccionado no existe!")
+def mark_completed():
+    chosen = task_list.curseselection()
+    if not chosen:
+        messagebox.showwarning("Warning", "Please select a task from the list, the one selected does not exist!")
         return
 
-    texto = lista_tareas.get(seleccion[0])
-    id_tarea = int(texto.split(".")[0])
-    for tarea in tareas:
-        if tarea['id'] == id_tarea:
-            tarea['completado'] = True
-            guardar_tareas()
-            actualizar_lista()
-            messagebox.showinfo("Hecho", "La tarea se ha marcado como completa")
+    text = task_list.get(chosen[0])
+    id_task= int(text.split(".")[0])
+    for task in tasks:
+        if task['id'] == id_task:
+            task['completed'] = True
+            save_tasks()
+            update_task()
+            messagebox.showinfo("Completed", "Taks has been marked as completed.")
             return
 
-def eliminar_tarea():
-    global tareas
-    seleccion = lisat_tareas.curseselection()
-    if not seleccion:
-        messagebox.showwarning("Atencion", "Seleccione una tarea para eliminar (Este cambio no se puede restaurar)")
+def delete_task():
+    global tasks
+    chosen = task_list.curseselection()
+    if not chosen:
+        messagebox.showwarning("Warning", "Select a task to delete (This change can not be undone)")
         return
     
-    texto = lista_tareas.get(seleccion[0])
-    id_tarea = int(texto.split(".")[0])
+    text = task_list.get(chosen[0])
+    id_task = int(text.split(".")[0])
 
-    if messagebox.askyesno("Confirmar", f"Procederemos a eliminar la tarea {id_tarea}"):
-        tareas_filtrada = [tarea for tarea in tareas if tarea['id'] != id_tarea]
-        guardar_tareas()
-        actualizar_lista()
+    if messagebox.askyesno("Confirmation", f"Are you sure you want to delete task with #{id_tarea}?"):
+        filtered_tasks = [task for task in tasks if task['id'] != id_task]
+        save_tasks()
+        update_task()
 
-def busqueda_tarea():
-    valor_busqueda = simpledialog.askstring("Buscar", "Ingrese la palabra clave:")
-    if valor_busqueda:
-        actualizar_lista(filtro_busqueda=valor_busqueda.lower())
+def task_search():
+    search_value = simpledialog.askstring("Search", "Enter the search value:")
+    if search_value:
+        update_task(search_filter=search_value.lower())
 
 
-def filtrar_categoria():
-    categoria = simpledialog.askstring("Filtrar", "Escribe la categoria a filtrar:")
-    if categoria:
-        actualizar_lista(filtro_categoria=categoria.capitalize())
+def category_filter():
+    category = simpledialog.askstring("Filter", "Type the category to filter:")
+    if category:
+        update_task(cat_filter=category.capitalize())
 
-def actualizar_lista(mostrar_tareas=True, filtro_busqueda=None, filtrar_categoria=None):
-    lista_tareas.delete(0,tk.END)
-    for tarea in tareas:
-        if not mostrar_tareas and tarea["compleatdo"]:
+def update_task(show_tasks=True, search_filter=None, cat_filter=None):
+    task_list.delete(0,tk.END)
+    for task in tasks:
+        if not show_tasks and task["completed"]:
             continue
-        if filtro_busqueda and filtro_busqueda not in tarea["descripcion"].lower():
+        if search_filter and search_filter not in task["description"].lower():
             continue
-        if filtrar_categoria and filtrar_categoria != tarea["categorias"]:
+        if cat_filter and cat_filter != task["category"]:
             continue
 
-        limite = date.fromisoformat(tarea["finalizar"])
-        vencida = "⚠️ Vencida :(" if limite < hoy and not tarea["completado"] else ""
-        estado = "✅" if tarea["completado"] else "⏳"
+        finish = date.fromisoformat(task["completion"])
+        expired = "⚠️ Expired! :(" if finish < today and not task["completed"] else ""
+        status = "✅" if task["completed"] else "⏳"
 
-        linea = f"{tarea["id"]}, {estado} {tarea["descripcion"]} [{tarea["prioridad"]}] - {tarea["categorias"]} | Vence {tarea["finalizar"]} {vencida}"
-        lista_tareas.insert(tk.END, linea)
+        row = f"{task["id"]}, {status} {task["description"]} [{task["priority"]}] - {task["category"]} | Vence {task["completion"]} {expired}"
+        task_list.insert(tk.END, row)
 
-        if vencida:
-            lista_tareas.itemconfig(tk.END, {'fg': 'red'})
-        elif tarea["completado"]:
-            lista_tareas.itemconfig(tk.END, {'fg': 'gray'})
+        if expired:
+            task_list.itemconfig(tk.END, {'fg': 'red'})
+        elif task["completed"]:
+            task_list.itemconfig(tk.END, {'fg': 'gray'})
 
-def menu_principal():
-    global lista_tareas
+def main_menu():
+    global task_list
     root = tk.Tk()
-    root.title("Gestor de Tareas")
+    root.title("To Do App")
     root.geometry("700x550")
     root.configure(bg="#2c3e50")
 
@@ -169,19 +169,19 @@ def menu_principal():
 
     principal = tk.Frame(root, bg="#1a252f", pady=10)
     principal.pack(fill="x")
-    tk.Label(principal, text="MIS TAREAS", font=("Arial", 18, "bold"), bg="#1a252f", fg="#ecf0f1").pack()
+    tk.Label(principal, text="MY TASKS", font=("Arial", 18, "bold"), bg="#1a252f", fg="#ecf0f1").pack()
 
 
     menu_lateral = tk.Frame(root, bg="#2c3e50", padx=10, pady=10)
     menu_lateral.pack(side="left", fill="y")
 
-    tk.Button(menu_lateral, text="➕ Agregar Tarea", command=agregar_tarea, **style).pack(fill="x", pady=5)
-    tk.Button(menu_lateral, text="✔ Completar", command=marcar_completada, **style).pack(fill="x", pady=5)
-    tk.Button(menu_lateral, text="🔍 Buscar", command=busqueda_tarea, **style).pack(fill="x", pady=5)
-    tk.Button(menu_lateral, text="📂 Por Categoría", command=filtrar_categoria, **style).pack(fill="x", pady=5)
-    tk.Button(menu_lateral, text="🔄 Ver Todas", command=lambda: actualizar_lista(True), **style).pack(fill="x", pady=5)
-    tk.Button(menu_lateral, text="❌ Eliminar", command=eliminar_tarea, bg="#e74c3c", fg="white", font=("Arial", 10, "bold")).pack(fill="x", pady=20)
-    tk.Button(menu_lateral, text="🚪 Salir", command=root.destroy, bg="#95a5a6").pack(side="bottom", fill="x")
+    tk.Button(menu_lateral, text="➕ Add New Task", command=add_task, **style).pack(fill="x", pady=5)
+    tk.Button(menu_lateral, text="✔ Mark as Completed", command=mark_completed, **style).pack(fill="x", pady=5)
+    tk.Button(menu_lateral, text="🔍 Search", command=task_search, **style).pack(fill="x", pady=5)
+    tk.Button(menu_lateral, text="📂 By Category", command=category_filter, **style).pack(fill="x", pady=5)
+    tk.Button(menu_lateral, text="🔄 Show all", command=lambda: update_task(True), **style).pack(fill="x", pady=5)
+    tk.Button(menu_lateral, text="❌ Delete", command=delete_task, bg="#e74c3c", fg="white", font=("Arial", 10, "bold")).pack(fill="x", pady=20)
+    tk.Button(menu_lateral, text="🚪 Exit", command=root.destroy, bg="#95a5a6").pack(side="bottom", fill="x")
 
     frame_lista = tk.Frame(root, bg="#2c3e50", padx=10, pady=10)
     frame_lista.pack(side="right", expand=True, fill="both")
@@ -189,14 +189,14 @@ def menu_principal():
     scrollbar = tk.Scrollbar(frame_lista)
     scrollbar.pack(side="right", fill="y")
 
-    lista_tareas = tk.Listbox(frame_lista, font=("Consolas", 10), bg="#ecf0f1", yscrollcommand=scrollbar.set)
-    lista_tareas.pack(expand=True, fill="both")
-    scrollbar.config(command=lista_tareas.yview)
+    task_list = tk.Listbox(frame_lista, font=("Consolas", 10), bg="#ecf0f1", yscrollcommand=scrollbar.set)
+    task_list.pack(expand=True, fill="both")
+    scrollbar.config(command=task_list.yview)
 
-    cargar_tareas()
-    actualizar_lista()
+    load_tasks()
+    update_task()
     
     root.mainloop()
 
 if __name__ == "__main__":
-    menu_principal()  
+    main_menu()  
