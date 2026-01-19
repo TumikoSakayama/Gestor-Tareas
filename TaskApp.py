4abadeaf5fc0d6d401f354abb7012c0a7ca99b12
TumikoSakayama <glee310597@gmail.com>
2026-01-18 20:46:46 -0600

    Completed the rest of the buttons

--- TaskApp.py
+++ TaskApp.py
@@ -34,6 +34,36 @@ class TaskApp:
         btn_add = ttk.Button(input_frame, text="Add Task", command=self.add_button)
         btn_add.grid(rwo=0, column=4, padx=10, pady=5)
 
+        self.tree = ttk.Treeview(self.root, columns=("ID", "Desc", "Prio", "Cat", "Status", "End"), show='headings')
+        self.tree.heading("ID", text="ID")
+        self.tree.heading("Desc", text="Description")
+        self.tree.heading("Prio", text="Priority")
+        self.tree.heading("Cat", text-"Category")
+        self.tree.heading("Status", text="Status")
+        self.tree.heading("End", text="Deadline")
+
+        self.tree.column("ID", width=30)
+        self.tree.column("Status", width=80)
+        self.tree.pack(expand=True, fill="both", padx=10, pady=5)
+
+        btn_frame = ttk.Frame(self.root)
+        btn_frame.pack(fill="x", padx=10, pady=5)
+
+        ttk.Button(btn_frame, text="Mark Completed", command=self.mark_done).pack(side="left", padx=5)
+        ttk.Button(btn_frame, text="Delete Task", command=self.delete_task).pack(side="left", padx=5)
+
+    def refresh_table(self):
+        status = "✅ Done" if task.is_completed else ("⚠️ Expired" if task.is_expired())
+
+        self.tree.insert("", "end", values=(
+            task.task_id,
+            task.description,
+            task.priority,
+            task.category,
+            status,
+            task.end_date
+        ))
+
     def add_button(self):
         description = self.description_entry.get()
         priority = self.priority_combo.get()
@@ -44,5 +74,31 @@ class TaskApp:
             return
         
         self.manager.add_task(description, priority, category)
-        self.update_task_display()
-        self.description_entry.delete(0, 'end')
+        self.refresh_table()
+        self.description_entry.delete(0, tk.END)
+
+    def complete_button(self):
+        chosen = self.tree.selection()
+        if not chosen:
+            return
+        
+        id_selected = self.tree.item(chosen[0])
+        task_id = id_selected['values'][0]
+
+        task = self.manager.find_task_by_id(task_id)
+        if task:
+            task.mark_done()
+            self.manager.save_tasks()
+            self.refresh_table()
+
+    def delete_button(self):
+        chosen = self.tree.selection()
+        if not chosen:
+            return
+        
+        id_selected = self.tree.itme(chosen[0])
+        task_id = id_selected['values'][0]
+
+        if messagebox.askyesno("Confirm", f"Delete task {task_id}?"):
+            self.manager.delete_task(task_id)
+            self.refresh_table()
